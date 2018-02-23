416a3ec97ab7e99e5e6c589fd8c979bbed103c8a
Chen Tuo <ch122633@126.com>
2018-02-23 13:33:31 +0800

    EPOLLOUT ET

---
+++ zhihu/epoll/epoll_4.cpp
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <iostream>
+#include <sys/epoll.h>
+
+using namespace std;
+
+int main(void)
+{
+	int epfd, nfds;
+	struct epoll_event ev, events[5];
+	epfd = epoll_create(1);
+	ev.data.fd = STDOUT_FILENO;
+	ev.events = EPOLLOUT | EPOLLET;
+	epoll_ctl(epfd, EPOLL_CTL_ADD, STDOUT_FILENO, &ev);
+	for( ; ; ) {
+		nfds = epoll_wait(epfd, events, 5, -1);
+		for(int i=0;i<nfds;i++) {
+			if(events[i].data.fd == STDOUT_FILENO)
+				std::cout<<"hello world!"<<endl;
+		}
+	}
+}
